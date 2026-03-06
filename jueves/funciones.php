ca890f61013e9383ad4532b7344741783ac71451
klealufide <kleal40321@ufide.ac.cr>
2026-03-05 20:06:11 -0600

    Unidad 4

--- jueves/funciones.php
+++ jueves/funciones.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 $nombreEstudiante = "Maria";
 $edad = 22;
 $esEstudiante = true;
@@ -153,5 +153,156 @@ foreach ($listaFrutas as $frutas) {
 
 for ($i = 0; $i < count($listaFrutas); $i++) {
     // tenemos limite
-    echo $listaFrutas[$i]. $espacio;
+    echo $listaFrutas[$i] . $espacio;
+}
+
+echo "<hr>";
+echo $listaFrutas[1]    . $espacio;
+
+$persona = [
+    "nombre" => "Maria",
+    "apellido" => "Mora",
+    "edad" => 22,
+    "esEstudiante" => true
+];
+
+echo $persona["apellido"];
+
+echo $espacio;
+foreach ($persona as $index => $valor) {
+    echo  $index . ": " . $valor . $espacio;
+}
+
+
+
+
+$persona2 = [
+    "nombre" => "Juan",
+    "apellido" => "Mora",
+    "edad" => 50,
+    "esEstudiante" => false
+];
+
+$persona3 = [
+    "nombre" => "Pablo",
+    "apellido" => "Campos",
+    "edad" => 27,
+    "esEstudiante" => true
+];
+
+
+$listaPersonas = [
+    $persona,
+    $persona2,
+    $persona3
+];
+
+
+echo "<table border='1'><tr><td>Nombre</td><td>Apellido</td><td>Edad</td><td>Es estudiante?</td></tr>";
+
+foreach ($listaPersonas as $index => $valor) {
+    if ($valor["edad"] > 25) {
+        $esEstudiante = ($valor["esEstudiante"]) ? "Es estudiante" : "No es estudiante";
+        echo "<tr><td>" . $valor["nombre"] . "</td><td>" . $valor["apellido"] . "</td><td>" . $valor["edad"] . "</td><td>" .  $esEstudiante . "</td></tr>";
+    }
+}
+
+echo "</table>";
+
+
+print_r($listaPersonas);
+
+
+
+echo "<hr>";
+
+
+array_push($listaFrutas, "fresa");
+
+print_r($listaFrutas);
+
+
+$index = array_search("fresa", $listaFrutas);
+echo $espacio;
+echo $index;
+echo $espacio;
+$arreglo_unido = array_merge($listaFrutas, $listaPersonas);
+
+print_r($arreglo_unido);
+
+echo $espacio;
+
+echo $arreglo_unido[3];
+echo $espacio;
+echo $arreglo_unido[6]["nombre"];
+
+// Simple
+function sumar($a, $b)
+{
+    return $a + $b;
+}
+
+//Anonima
+$restar = function ($a, $b) {
+    return $a - $b;
+};
+
+//flecha
+
+$multiplicar = fn($a, $b) => $a * $b;
+
+$numeroA = 10;
+$numeroB = 5;
+
+echo sumar($numeroA, $numeroB);
+echo $espacio;
+echo $restar($numeroA, $numeroB);
+echo $espacio;
+echo $multiplicar($numeroA, $numeroB);
+echo $espacio;
+
+$archivo = fopen("archivo.txt", "w");
+$text = "Nuevo Hola mundo!\n";
+fwrite($archivo, $text);
+$text = "Otro Hola mundo!\n";
+fwrite($archivo, $text);
+fclose($archivo);
+
+
+$archivo = fopen("archivo.txt", "r");
+while (!feof($archivo)) {
+    echo fgets($archivo) . $espacio;
+}
+fclose($archivo);
+
+//variable reservada arreglo asociativo;
+print_r($_SESSION);
+
+$_SESSION["usuario"] = "kleal";
+$_SESSION["rol"] = "admin";
+$_SESSION["nombre"] = "Karol";
+echo $_SESSION["usuario"];
+
+echo "<a href='unidad4.php'> Ir a Unidad4</a>";
+echo $espacio;
+
+///////////////////////////////////////////Registro ///////////////////////////////////////
+
+// uusuario  kleal //contrasenna N 12345
+
+$clave = "12345";
+// $2y$10$h5xV.Y6SinFxAaimCnR5we4Ezo0XluhTBZDYTINBsU77jWvGRVz6i
+// $2y$10$dwR5.W5LzAq267ExW2fAyusQf9F1HnhdJ9w7E3qXTTkS8DL1RPcuO
+$clave_encriptada = password_hash($clave, PASSWORD_BCRYPT);
+
+
+
+///////////////////////////////////////////Login ///////////////////////////////////////
+
+$clave_basededatos = '$2y$10$h5xV.Y6SinFxAaimCnR5we4Ezo0XluhTBZDYTINBsU77jWvGRVz6i';
+
+if (password_verify($clave, $clave_basededatos)) {
+    echo "La clave coincide";
+} else {
+    echo "La clave no coincide";
 }
--- jueves/unidad4.php
+++ jueves/unidad4.php
@@ -1,16 +1,29 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <title>Unidad 4</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
+
 </head>
+
 <body>
     <?php
-        echo "<h1>Hola mundo!</h1>";
+    if (!empty($_SESSION)) {
+        if ($_SESSION["rol"] == "admin") {
+            echo "<b>Hola Administrador!, " . $_SESSION["nombre"] . "</b>";
+        } else {
+            echo "<b>Hola" . $_SESSION["nombre"] . "</b>";
+        }
+    } else {
+        echo "<b>Hola ud no esta logueado,</b>";
+    } ?>
 
-    ?>
-    <p>Esto es un parrafo</p>
+    <a href="funciones.php"> Ir a funciones</a>
+     <a href="logout.php"> Salir</a>
 </body>
+
 </html>
