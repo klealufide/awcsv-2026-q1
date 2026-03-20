4e72cf4812106900dec324200abf47a89596e72f
klealufide <kleal40321@ufide.ac.cr>
2026-03-19 19:18:38 -0600

    Unidad 6

---
+++ jueves/js/unidad6.js
@@ -0,0 +1,77 @@
+$(function () {
+    //JavaScript??
+    let titulo = document.getElementById("titulo");
+    let subtitulo = document.querySelectorAll(".subtitulo");
+    console.log(titulo);
+    console.log(subtitulo);
+    //Jquery??
+    titulo = $("#titulo");
+    subtitulo = $(".subtitulo");
+    etiqueta = $("li");
+    descendencia = $("form > input");
+    multiples = $("#titulo, h3, a");
+    console.log(titulo);
+    console.log(subtitulo);
+    console.log(etiqueta);
+    console.log(descendencia);
+    console.log(multiples);
+
+    let butonCambiarTexto = $("#cambiarTexto");
+
+    butonCambiarTexto.on("click", function () {
+        titulo.text("Nuevo texto");
+        subtitulo.html("<h3>Nuevo subtitulo</h3>");
+        subtitulo.css("font-size", "30px");
+        multiples.addClass("okay");
+        titulo.removeClass("okay");
+
+        let url = $("a");
+        url.attr("href", "https://api.jquery.com/click/");
+        url.attr("target", "_blank");
+
+        let img = $("img");
+
+        img.attr("src", "./img/images.jpg");
+    })
+
+    let formLogin = $("#formLogin");
+
+    formLogin.on("submit", function (event) {
+
+        let username = $("#username");
+        let password = $("#password");
+        let rol = $("#rol");
+        console.log(event)
+        if (username.val() == "" || password.val() == "" || rol.val() == "") {
+
+            event.preventDefault();
+            if (username.val() == "") {
+                username.addClass("error");
+                username.removeClass("okay");
+            } else {
+                username.removeClass("error");
+                username.addClass("okay");
+            }
+
+            if (password.val() == "") {
+                password.addClass("error");
+                password.removeClass("okay");
+            } else {
+                password.removeClass("error");
+                password.addClass("okay");
+            }
+
+            if (rol.val() == "") {
+                rol.addClass("error");
+                rol.removeClass("okay");
+            } else {
+                rol.removeClass("error");
+                rol.addClass("okay");
+            }
+        } else {
+            this.submit();
+        }
+
+
+    })
+})
