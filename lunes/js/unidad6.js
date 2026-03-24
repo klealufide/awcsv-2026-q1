ed0c717419e33d4efae8de548223ccaae676e02f
klealufide <kleal40321@ufide.ac.cr>
2026-03-23 19:01:00 -0600

    unidada 6 parte2

--- lunes/js/unidad6.js
+++ lunes/js/unidad6.js
@@ -13,29 +13,35 @@ $(function () {
     let multiple = $("h1, ul, .subtitulo");
     console.log(multiple);
 
-    $("#formLogin").on("submit", function (event) {
+    let formLogin = $("#formLogin");
+
+    formLogin.on("submit", function (event) {
         event.preventDefault();
         let username = $("#username");
         let password = $("#password");
         let rol = $("#rol");
         let error = 0;
         if (username.val() == "") {
-            username.css("border-color","red");
+            username.css("border-color", "red");
             error++;
         }
         if (password.val() == "") {
-            password.css("border-color","red");
+            password.css("border-color", "red");
             error++;
         }
 
         if (rol.val() == "") {
-            rol.css("border-color","red");
+            rol.css("border-color", "red");
             error++;
         }
 
         if (error == 0) {
             alert("Envio correctamente!");
+        } else {
+            this.submit();
         }
+
+
     });
 
     $("#cambiarTexto").on("click", function (event) {
@@ -53,8 +59,73 @@ $(function () {
         })
 
         $("img").attr("src", "./img/logo.jpg");
-        $("a").attr("href", "https://api.jquery.com/submit/")
+        $("a").attr("href", "https://api.jquery.com/submit/");
+        $("a").attr("target", "_blank")
     });
 
+    let btnDesaparecer = $("#desaparecer");
+    let btnAparecer = $("#aparecer");
+    let btnAgrandar = $("#agrandar");
+    let btnDismunir = $("#disminuir");
+    let btnCmbiarColor = $("#cambiarColor");
+    let cuadrado = $("#cuadrado");
 
+    let btnCambiarImagen = $("#cambiarImagen");
+
+    btnDesaparecer.on("click", function () {
+        cuadrado.fadeOut(1000);
+    })
+
+    btnAparecer.on("click", function () {
+        cuadrado.fadeIn(1000);
+    })
+
+    btnCmbiarColor.on("click", function () {
+        cuadrado.toggleClass("rosado");
+    })
+
+    btnAgrandar.on("click", function () {
+        cuadrado.animate({
+            width: '500px',
+            height: '500px',
+            opacity: 0.5
+        }, 1000);
+    })
+
+    btnDismunir.on("click", function () {
+        cuadrado.animate({
+            width: '100px',
+            height: '100px',
+            opacity: 0.8
+        }, 1000);
+    })
+
+
+    btnCambiarImagen.on("click", function () {
+        $("img").animate({ opacity: 0.5 }, 500, function () {
+            $(this).attr("src", "./img/logo.jpg").animate({ opacity: 1 }, 500)
+        });
+    })
+
+
+    let formTarea = $("#formTareas");
+    let mensaje = $("#mensaje");
+
+    mensaje.hide();
+
+    formTarea.on("submit", function (event) {
+        event.preventDefault();
+        let tarea = $("#tarea");
+        let listaTareas = $("#listaTareas");
+
+        if (tarea.val() == "") {
+            mensaje.show();
+            tarea.css("border-color", "red");
+        } else {
+            mensaje.hide();
+            listaTareas.prepend("<li>" + tarea.val() + "</li>")
+            tarea.css("border-color", "black");
+            tarea.val("")
+        }
+    })
 })
