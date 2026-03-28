944bb95235e8fe9fb975b4d944ee216588e63ba8
klealufide <kleal40321@ufide.ac.cr>
2026-03-27 20:03:41 -0600

    reservacion BE y FE

--- viernes/reservaciones/app/views/reservations/index.php
+++ viernes/reservaciones/app/views/reservations/index.php
@@ -8,14 +8,15 @@
     <link
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
         rel="stylesheet">
-
+    <script src="public/js/jquery-4.0.0.min.js"></script>
+    <script src="public/js/reservation.js"></script>
 </head>
 
 <body class="container mt-5">
 
     <h2>Crear Reserva</h2>
 
-    <form action="/awcsv-2026-q1/jueves/reservaciones/index.php" method="POST">
+    <form id="formReservation">
 
         <input
             class="form-control mb-2"
@@ -41,7 +42,6 @@
             name="comments"
             id="comments"
             placeholder="Comentarios"></textarea>
-        <input type="hidden" name="option" value="store">
         <button class="btn btn-primary">
             Reservar
         </button>
@@ -63,26 +63,12 @@
             </tr>
         </thead>
 
-        <tbody>
+        <tbody id="listaReservaciones">
 
-            <?php if (!empty($reservations)) {
-                foreach ($reservations as $r): ?>
-
-                    <tr>
-                        <td><?= $r['name'] ?></td>
-                        <td><?= $r['reservation_date'] ?></td>
-                        <td><?= $r['people'] ?></td>
-                        <td><?= $r['comments'] ?></td>
-                    </tr>
-
-            <?php endforeach;
-            } ?>
 
         </tbody>
 
     </table>
-
-    <script src="/js/validation.js"></script>
 
 </body>
 
